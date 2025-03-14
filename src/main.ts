8a7e7ee514571dc73f85ee514dc3522e7185a61b
nikimasek <niki.masek@icloud.com>
2025-03-14 22:35:45 +0100

    initialize project structure with Vite, TypeScript, and basic HTML template

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import van from "vanjs-core";
+
+function App() {
+    return "kominikátor"
+}
+
+van.add(document.body, App);
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    root: path.resolve(__dirname, 'src'),
+    build: {
+        modulePreload: false,
+        emptyOutDir: true,
+        outDir: path.resolve(__dirname, 'dist'),
+        assetsDir: './',
+        target: ['es2020']
+    }
+});
