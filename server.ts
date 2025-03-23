2c9c8e0af47b78f4e4d6428cbfd5c24bc17b1450
nikimasek <niki.masek@icloud.com>
2025-03-23 21:15:56 +0100

    add file upload functionality for plates, integrate formidable for parsing, and remove obsolete panel.zip

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { readdir } from 'fs/promises';
+import { IncomingForm } from 'formidable';
+import { readdir, readFile, writeFile } from 'fs/promises';
 import path from 'path';
 const __dirname = import.meta.dirname;
 
@@ -16,6 +17,18 @@ app.get('/plates/:plate', (req, res) => {
     else
         res.sendStatus(404);
 });
+
+app.post('/plates/:plate', (req, res) => {
+    new IncomingForm().parse(req, async (err, fields, files) => {
+        const [file] = files!.panel!;
+        await writeFile(
+            path.resolve(__dirname, 'src/public/plates', req.params.plate + '.zip'),
+            await readFile(file.filepath)
+        );
+        res.send('ok');
+    });
+});
+
 app.get('/voice', (req, res) => {
     fetch(`https://translate.google.com/translate_tts?tl=cs-CZ&q=${encodeURIComponent((req.query as any).text)}&client=tw-ob`)
         .then(x => x.blob())
